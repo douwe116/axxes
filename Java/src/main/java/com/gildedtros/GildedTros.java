b57f84573d9ef33561308fd14bf0de82014d4ed8
Douwe Visser <douwe.visser@keylane.com>
2024-03-22 17:43:25 +0100

    Rewrite the current methods

--- Java/src/main/java/com/gildedtros/GildedTros.java
+++ Java/src/main/java/com/gildedtros/GildedTros.java
@@ -7,58 +7,65 @@ class GildedTros {
         this.items = items;
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Good Wine")
-                    && !items[i].name.equals("Backstage passes for Re:Factor")
-                    && !items[i].name.equals("Backstage passes for HAXX"))
-            {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("B-DAWG Keychain")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+    public void updateInventory() {
+        for (Item item: items) {
+            updateInventory(item);
+            updateExpiration(item);
+            processExpiration(item);
+        }
+    }
+
+    private void updateInventory(Item item) {
+        if(item.name.equals("Good Wine")) {
+            increaseQuality(item);
+        } else if (item.name.equals("Backstage passes for Re:Factor") || item.name.equals("Backstage passes for HAXX")){
+            increaseQuality(item);
+            if (item.sellIn < 11) {
+                increaseQuality(item);
+            }
+            if (item.sellIn < 6) {
+                increaseQuality(item);
+            }
+        } else if (item.name.equals("B-DAWG Keychain")) {
+            return;
+        } else {
+            decreaseQuality(item);
+        }
+    }
+
+    private void updateExpiration(Item item) {
+        if (item.name.equals("B-DAWG Keychain")) {
+            return;
+        }
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private void processExpiration(Item item) {
+        if (item.sellIn < 0) {
+            if (item.name.equals("Good Wine")) {
+                increaseQuality(item);
+            } else if (item.name.equals("Backstage passes for Re:Factor") || item.name.equals("Backstage passes for HAXX")) {
+                item.quality = 0;
+            }
+            else if (item.name.equals("B-DAWG Keychain")) {
+                return;
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes for Re:Factor") || items[i].name.equals("Backstage passes for HAXX") ) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+                decreaseQuality(item);
             }
+        }
+    }
 
-            if (!items[i].name.equals("B-DAWG Keychain")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+    private void decreaseQuality(Item item) {
+        updateInventory(item, item.quality > 0, -1);
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Good Wine")) {
-                    if (!items[i].name.equals("Backstage passes for Re:Factor") && !items[i].name.equals("Backstage passes for HAXX")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("B-DAWG Keychain")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+    private void increaseQuality(Item item) {
+        updateInventory(item, item.quality < 50, 1);
+    }
+
+    private void updateInventory(Item item, boolean b, int i) {
+        if (b) {
+            item.quality = item.quality + i;
         }
     }
 }
--- Java/src/test/java/com/gildedtros/GildedTrosTest.java
+++ Java/src/test/java/com/gildedtros/GildedTrosTest.java
@@ -10,7 +10,7 @@ class GildedTrosTest {
     void nonNegativeQuality() {
         Item[] items = new Item[] { new Item("itemName", 0, 0) };
         GildedTros app = new GildedTros(items);
-        app.updateQuality();
+        app.updateInventory();
         assertEquals("itemName", app.items[0].name);
         assertEquals(-1, app.items[0].sellIn);
         assertEquals(0, app.items[0].quality);
@@ -19,7 +19,7 @@ class GildedTrosTest {
     void qualityDegarationWhenBad() {
         Item[] items = new Item[] { new Item("itemName", 0, 10) };
         GildedTros app = new GildedTros(items);
-        app.updateQuality();
+        app.updateInventory();
         assertEquals("itemName", app.items[0].name);
         assertEquals(-1, app.items[0].sellIn);
         assertEquals(8, app.items[0].quality);
@@ -28,7 +28,7 @@ class GildedTrosTest {
     void qualityDegarationWhenGood() {
         Item[] items = new Item[] { new Item("itemName", 10, 10) };
         GildedTros app = new GildedTros(items);
-        app.updateQuality();
+        app.updateInventory();
         assertEquals("itemName", app.items[0].name);
         assertEquals(9, app.items[0].sellIn);
         assertEquals(9, app.items[0].quality);
@@ -37,7 +37,7 @@ class GildedTrosTest {
     void goodWine() {
         Item[] items = new Item[] { new Item("Good Wine", 10, 10) };
         GildedTros app = new GildedTros(items);
-        app.updateQuality();
+        app.updateInventory();
         assertEquals("Good Wine", app.items[0].name);
         assertEquals(9, app.items[0].sellIn);
         assertEquals(11, app.items[0].quality);
@@ -46,7 +46,7 @@ class GildedTrosTest {
     void maxQuality() {
         Item[] items = new Item[] { new Item("Good Wine", 10, 50) };
         GildedTros app = new GildedTros(items);
-        app.updateQuality();
+        app.updateInventory();
         assertEquals("Good Wine", app.items[0].name);
         assertEquals(9, app.items[0].sellIn);
         assertEquals(50, app.items[0].quality);
@@ -55,7 +55,7 @@ class GildedTrosTest {
     void keychain() {
         Item[] items = new Item[] { new Item("B-DAWG Keychain", 10, 80) };
         GildedTros app = new GildedTros(items);
-        app.updateQuality();
+        app.updateInventory();
         assertEquals("B-DAWG Keychain", app.items[0].name);
         assertEquals(10, app.items[0].sellIn);
         assertEquals(80, app.items[0].quality);
@@ -64,7 +64,7 @@ class GildedTrosTest {
     void backstagePasses10Days() {
         Item[] items = new Item[] { new Item("Backstage passes for Re:Factor", 9, 10) };
         GildedTros app = new GildedTros(items);
-        app.updateQuality();
+        app.updateInventory();
         assertEquals("Backstage passes for Re:Factor", app.items[0].name);
         assertEquals(8, app.items[0].sellIn);
         assertEquals(12, app.items[0].quality);
@@ -73,7 +73,7 @@ class GildedTrosTest {
     void backstagePasses5Days() {
         Item[] items = new Item[] { new Item("Backstage passes for HAXX", 4, 10) };
         GildedTros app = new GildedTros(items);
-        app.updateQuality();
+        app.updateInventory();
         assertEquals("Backstage passes for HAXX", app.items[0].name);
         assertEquals(3, app.items[0].sellIn);
         assertEquals(13, app.items[0].quality);
@@ -82,7 +82,7 @@ class GildedTrosTest {
     void backstagePasses0Days() {
         Item[] items = new Item[] { new Item("Backstage passes for HAXX", 0, 10) };
         GildedTros app = new GildedTros(items);
-        app.updateQuality();
+        app.updateInventory();
         assertEquals("Backstage passes for HAXX", app.items[0].name);
         assertEquals(-1, app.items[0].sellIn);
         assertEquals(0, app.items[0].quality);
